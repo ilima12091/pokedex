fd2cc3b6866308057590b8b49a52e026471f7dc3
Maria-Gomez <mariajosegomez1994@gmail.com>
2024-11-21 17:18:22 -0300

    navegación de home al pokedex

--- app/src/main/java/com/example/pokedex/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/pokedex/ui/screens/HomeScreen.kt
@@ -10,17 +10,24 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.Logout
 import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun HomeTopBar() {
+fun HomeTopBar(
+    onNavigateToPokemonList: () -> Unit,
+) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -45,12 +52,28 @@ fun HomeTopBar() {
                 colorResource(id = android.R.color.white)
             )
         }
+        TextButton(
+            onClick = {
+                onNavigateToPokemonList()
+            },
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text("See all pokemon", fontSize = 18.sp, color = Color.Red)
+            }
+        }
     }
 }
 
 @Composable
-fun HomeScreen(){
-
+fun HomeScreen(
+    onNavigateToPokemonList: () -> Unit,
+){
+    HomeTopBar(
+        onNavigateToPokemonList = onNavigateToPokemonList
+    )
 }
 
 
