84045d3ada68eb470b0eedf9dde085ad4b055937
Marcelo de Leon <mdeleoncosentino@gmail.com>
2024-11-22 20:45:58 -0300

    add DI to home view model

--- app/src/main/java/com/example/pokedex/ui/viewModels/HomeViewModel.kt
+++ app/src/main/java/com/example/pokedex/ui/viewModels/HomeViewModel.kt
@@ -9,12 +9,14 @@ import com.example.pokedex.data.FirestoreUserRepository
 import com.example.pokedex.data.PokemonRepository
 import com.example.pokedex.ui.utils.Constants
 import com.example.pokedex.ui.utils.getPokemonIdsForHomeScreen
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 data class HomeUiState(
     val isLoading: Boolean = false,
@@ -22,9 +24,12 @@ data class HomeUiState(
     val pokemonDetailsList: List<FetchPokemonDetailsResponse> = emptyList()
 )
 
-class HomeViewModel: ViewModel() {
-    private val firebaseAuthRepository = FirebaseAuthRepository()
-    private val firestoreUserRepository = FirestoreUserRepository()
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val firebaseAuthRepository: FirebaseAuthRepository,
+    private val firestoreUserRepository: FirestoreUserRepository,
+    private val pokemonRepository: PokemonRepository,
+) : ViewModel() {
     private val _uiState = MutableStateFlow(HomeUiState())
     val uiState = _uiState.asStateFlow()
 
@@ -49,7 +54,7 @@ class HomeViewModel: ViewModel() {
 
                 val pokemonDetailsList = pokemonIds.map { id ->
                     async {
-                        PokemonRepository.fetchPokemonDetails(id)
+                        pokemonRepository.fetchPokemonDetails(id)
                     }
                 }.awaitAll()
 
